4e95f90636cea70368cb0bd44988e065018c7f89
Victoria Mshetsyan <79749519+vimshetsyan@users.noreply.github.com>
2022-11-01 20:22:54 +0400

    Update timer.py

--- timer.py
+++ timer.py
@@ -2,16 +2,17 @@ import time
 
 
 def countdown(count):
-    while count:
-        minutes, seconds = divmod(count, 60)
-        timer = '{:02d}:{:02d}'.format(minutes, seconds)
-        print(timer)
-        time.sleep(1)
-        count -= 1
+    while count:   # it is to say while 'count' > 0 or is True
+        minutes, seconds = divmod(count, 60)  # Divmod display the quotient and the remainder of 'count' divided by 60:
+        timer = '{:02d}:{:02d}'.format(minutes, seconds)  # Formatting timer to be visually more understandable
+        print(timer)  # printing remaining time
+        time.sleep(1)  # it waits a second because we need to countdown every second
+        count -= 1  # we need to subtract 1 from the initial second's /
+        # value because we just coded the program to sleep 1 second
 
-    print('Your time is up')
+    print('Your time is up') 
 
 
-count_down = input("Enter the time in seconds: ")
+count_down = input("Enter the time in seconds: ")  # inputting value as seconds
 
-countdown(int(count_down))
+countdown(int(count_down))  # assigning value of 'count_down' to 'countdown' function
